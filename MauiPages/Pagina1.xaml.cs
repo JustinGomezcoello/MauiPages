ac462af26e9cbb2e0b944efad2e12ffc2e57075e
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2024-11-21 09:05:02 -0500

    archivos crear

--- MauiPages/Pagina1.xaml.cs
+++ MauiPages/Pagina1.xaml.cs
@@ -1,22 +1,30 @@
+using MauiPages.Modelos;
 using MauiPages.Repositories;
 
 namespace MauiPages;
 
 public partial class Pagina1 : ContentPage
 {
+	Estudiante _estudiante;
 	public Pagina1()
 	{
 		InitializeComponent();
 		EstudinateUDLARepository estudinateUDLARepository = new EstudinateUDLARepository();
-		var infoEstudiante= estudinateUDLARepository.DevuelveInfoEstudainte();
+		 _estudiante = estudinateUDLARepository.DevuelveInfoEstudainte();
+		BindingContext= _estudiante;
 
-		Label_ID.Text = infoEstudiante.Id.ToString();
-		Label_nombre.Text = infoEstudiante.Nombre;
-		Label_carrera.Text = infoEstudiante.Carrera;
-
-
-
+		//Label_ID.Text = infoEstudiante.Id.ToString();
+		//Label_nombre.Text = infoEstudiante.Nombre;
+		//Label_carrera.Text = infoEstudiante.Carrera;
 
 
+
+
+
+    }
+
+    private void CambiaNombre_Clicked(object sender, EventArgs e)
+    {
+		_estudiante.Nombre = "Cambiar Nombre";
     }
 }
--- MauiPages/Repositories/EstudinateUDLARepository.cs
+++ MauiPages/Repositories/EstudinateUDLARepository.cs
@@ -1,14 +1,17 @@
 ﻿using MauiPages.Modelos;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MauiPages.Repositories
 {
     public class EstudinateUDLARepository
     {
+        public string _fileName = Path.Combine(FileSystem.AppDataDirectory, "estudiante.txt");
         public Estudiante DevuelveInfoEstudainte()
         {
             return new Estudiante()
@@ -18,6 +21,25 @@ namespace MauiPages.Repositories
                 Carrera = "Ing de Software"
 
             };
+
+            
+
+        }
+
+        public void GuardarEnArchivo(Estudiante estudiante)
+        {
+            string json= JsonConvert.SerializeObject(estudiante);
+            File.WriteAllText(_fileName, estudiante.Nombre);
+        }
+        public Estudiante ObtenerInformacionArchivo()
+        {
+            Estudiante estudiante = new Estudiante();
+            if(File.Exists(_fileName))
+            {
+                string data_archivo = File.ReadAllText(_fileName);
+                estudiante = JsonConvert.DeserializeObject<Estudiante>(data_archivo);
+            }
+            return estudiante;
         }
     }
 }
